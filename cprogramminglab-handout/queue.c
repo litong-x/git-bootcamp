eb5e3b1f9419306dc233a58b47f22a45c31fa174
Litong Xiao <litongx@angelshark.ics.cs.cmu.edu>
2019-01-22 01:28:20 -0500

    insert a relevant commit message here

--- cprogramminglab-handout/queue.c
+++ cprogramminglab-handout/queue.c
@@ -48,8 +48,9 @@ void q_free(queue_t *q)
 bool q_insert_head(queue_t *q, char *s)
 {
     list_ele_t *newh;
-    /* What should you do if the q is NULL? */
+    if(!q) return false;/* What should you do if the q is NULL? */
     newh = malloc(sizeof(list_ele_t));
+    if (!newh) return false;
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
     newh->next = q->head;
